accb02d78788fb8b5f59397483d1f3fdc8c1a8b2
Dobrianska <tsisinska.a@gmail.com>
2023-02-03 21:02:15 +0200

    JS. Homework. Lesson 1

--- JS/lesson 1/homework/main.js
+++ JS/lesson 1/homework/main.js
@@ -66,10 +66,10 @@
 // Додаткове для тих хто цікавився prompt`oм
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
 
-let firstName = prompt(`enter firstName`);
-let middleName = prompt('enter middleName');
-let age = prompt('enter age');
-
-let res = `${firstName} ${middleName} ${age}`;
-console.log(res);
+// let firstName = prompt(`enter firstName`);
+// let middleName = prompt('enter middleName');
+// let age = prompt('enter age');
+//
+// let res = `${firstName} ${middleName} ${age}`;
+// console.log(res);
 
